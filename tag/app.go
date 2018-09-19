0c889099dc8bb781bd414d7be6e34dee60428228
hailong11 <hailong11@staff.weibo.com>
2018-09-19 16:29:33 +0800

    update

--- tag/app.go
+++ tag/app.go
@@ -10,18 +10,6 @@ import (
 	"github.com/hailongz/kk-micro/micro"
 )
 
-func Open(app micro.IApp) micro.IApp {
-
-	{
-		s := TagService{}
-		app.Use(&TagQueryTask{}, &s)
-		app.Use(&CounterGetTask{}, &s)
-		app.Use(&CounterQueryTask{}, &s)
-	}
-
-	return app
-}
-
 func Add(conn db.Database, prefix string, eid int64, tag string) error {
 
 	v := Tag{}
@@ -208,4 +196,216 @@ func QuerySQL(prefix string, tags string, args []interface{}) (string, []interfa
 	}
 
 	return sql.String(), args
+}
+
+func TagQuery(conn db.Database, prefix string, task *TagQueryTask) error {
+
+	var err error = nil
+
+	var v = Tag{}
+
+	sql := bytes.NewBuffer(nil)
+
+	args := []interface{}{}
+
+	sql.WriteString(" WHERE eid=?")
+
+	args = append(args, task.Eid)
+
+	if task.OrderBy == "asc" {
+		sql.WriteString(" ORDER BY id ASC")
+	} else {
+		sql.WriteString(" ORDER BY id DESC")
+	}
+
+	var pageIndex = task.PageIndex
+	var pageSize = task.PageSize
+
+	if pageIndex < 1 {
+		pageIndex = 1
+	}
+
+	if pageSize < 1 {
+		pageSize = 10
+	}
+
+	if task.Counter {
+		var counter = micro.Counter{}
+		counter.PageIndex = pageIndex
+		counter.PageSize = pageSize
+		counter.RowCount, err = db.Count(conn, &v, prefix, sql.String(), args...)
+		if err != nil {
+			return err
+		}
+		if counter.RowCount%pageSize == 0 {
+			counter.PageCount = counter.RowCount / pageSize
+		} else {
+			counter.PageCount = counter.RowCount/pageSize + 1
+		}
+		task.Result.Counter = &counter
+	}
+
+	sql.WriteString(fmt.Sprintf(" LIMIT %d,%d", (pageIndex-1)*pageSize, pageSize))
+
+	var scanner = db.NewScaner(&v)
+	var vs = []Tag{}
+
+	rows, err := db.Query(conn, &v, prefix,
+		sql.String(), args...)
+
+	if err != nil {
+		return err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+
+		err = scanner.Scan(rows)
+
+		if err != nil {
+			return err
+		}
+
+		vs = append(vs, v)
+	}
+
+	task.Result.Tags = vs
+
+	return nil
+}
+
+func CounterQuery(conn db.Database, prefix string, task *CounterQueryTask) error {
+
+	if task.Tags == "" {
+		return micro.NewError(ERROR_NOT_FOUND, "未找到标签")
+	}
+
+	var err error = nil
+
+	var v = Counter{}
+
+	sql := bytes.NewBuffer(nil)
+
+	args := []interface{}{}
+
+	sql.WriteString(" WHERE tag IN (")
+
+	for i, v := range strings.Split(task.Tags, ",") {
+		if i != 0 {
+			sql.WriteString(",")
+		}
+		sql.WriteString("?")
+		args = append(args, v)
+	}
+
+	sql.WriteString(")")
+
+	if task.OrderBy == "asc" {
+		sql.WriteString(" ORDER BY id ASC")
+	} else {
+		sql.WriteString(" ORDER BY id DESC")
+	}
+
+	var pageIndex = task.PageIndex
+	var pageSize = task.PageSize
+
+	if pageIndex < 1 {
+		pageIndex = 1
+	}
+
+	if pageSize < 1 {
+		pageSize = 10
+	}
+
+	if task.Counter {
+		var counter = micro.Counter{}
+		counter.PageIndex = pageIndex
+		counter.PageSize = pageSize
+		counter.RowCount, err = db.Count(conn, &v, prefix, sql.String(), args...)
+		if err != nil {
+			return err
+		}
+		if counter.RowCount%pageSize == 0 {
+			counter.PageCount = counter.RowCount / pageSize
+		} else {
+			counter.PageCount = counter.RowCount/pageSize + 1
+		}
+		task.Result.Counter = &counter
+	}
+
+	sql.WriteString(fmt.Sprintf(" LIMIT %d,%d", (pageIndex-1)*pageSize, pageSize))
+
+	var scanner = db.NewScaner(&v)
+	var vs = []Counter{}
+
+	rows, err := db.Query(conn, &v, prefix,
+		sql.String(), args...)
+
+	if err != nil {
+		return err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+
+		err = scanner.Scan(rows)
+
+		if err != nil {
+			return err
+		}
+
+		vs = append(vs, v)
+	}
+
+	task.Result.Items = vs
+
+	return nil
+}
+
+func CounterGet(conn db.Database, prefix string, task *CounterGetTask) error {
+
+	if task.Tag == "" {
+		return micro.NewError(ERROR_NOT_FOUND, "未找到标签")
+	}
+
+	var err error = nil
+
+	var v = Counter{}
+
+	sql := bytes.NewBuffer(nil)
+
+	args := []interface{}{}
+
+	sql.WriteString(" WHERE tag =?")
+
+	args = append(args, task.Tag)
+
+	var scanner = db.NewScaner(&v)
+
+	rows, err := db.Query(conn, &v, prefix,
+		sql.String(), args...)
+
+	if err != nil {
+		return err
+	}
+
+	defer rows.Close()
+
+	if rows.Next() {
+
+		err = scanner.Scan(rows)
+
+		if err != nil {
+			return err
+		}
+
+		task.Result.Count = v.Count
+
+	} else {
+		task.Result.Count = 0
+	}
+
+	return nil
 }
